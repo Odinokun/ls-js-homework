bede42553c681981c89105ff559d85c9b10d8092
odinokun <odinokun@gmail.com>
2018-11-04 15:36:10 +0200

    trouble task#4

--- src/index.js
+++ src/index.js
@@ -72,7 +72,8 @@ function findAllPSiblings(where) {
 /*
  Задание 4:
 
- Функция представленная ниже, перебирает все дочерние узлы типа "элемент" внутри узла переданного в параметре where и возвращает массив из текстового содержимого найденных элементов
+ Функция представленная ниже, перебирает все дочерние узлы типа "элемент" внутри узла переданного
+  в параметре where и возвращает массив из текстового содержимого найденных элементов
  Но похоже, что в код функции закралась ошибка и она работает не так, как описано.
 
  Необходимо найти и исправить ошибку в коде так, чтобы функция работала так, как описано выше.
@@ -87,11 +88,21 @@ function findAllPSiblings(where) {
    findError(document.body) // функция должна вернуть массив с элементами 'привет' и 'loftschool'
  */
 function findError(where) {
-    var result = [];
+    let result = [];
 
-    for (var child of where.childNodes) {
-        result.push(child.innerText);
+    console.log('===== AHTUNG 2 =====', where.textContent);
+
+    for (let child of where.childNodes) {
+        result.push(child.textContent);
     }
+
+    // for (let i = 0; i < where.childNodes.length; i++) {
+    //     // result.push(where.childNodes[i].innerHTML);
+    //     result.push(where.childNodes[i].textContent);
+    //     // console.log('===== AHTUNG =====', where.childNodes.innerText);
+    // }
+
+    console.log('===== AHTUNG res =====', result);
 
     return result;
 }
@@ -188,7 +199,7 @@ export {
     createDivWithText,
     prepend,
     findAllPSiblings,
-    // findError,
+    findError,
     // deleteTextNodes,
     // deleteTextNodesRecursive,
     // collectDOMStat,
