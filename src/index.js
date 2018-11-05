156f11502b6c0f823b939f4cd1f8e688a9179ab8
odinokun <odinokun@gmail.com>
2018-11-05 19:22:32 +0200

    did task#3

--- src/index.js
+++ src/index.js
@@ -42,6 +42,13 @@ function removeListener(eventName, target, fn) {
    к переходу на другую страницу
  */
 function skipDefault(eventName, target) {
+
+    target.addEventListener(eventName, function (e) {
+
+        e.preventDefault(eventName);
+
+    });
+
 }
 
 /*
@@ -68,6 +75,17 @@ function emulateClick(target) {
    который будет вызывать указанную функцию только если кликнули на кнопку (элемент с тегом button)
  */
 function delegate(target, fn) {
+
+    // target.addEventListener('click', function () {
+    //
+    //     let btn = document.querySelector('button');
+    //
+    //     if (this === btn) {
+    //         fn();
+    //     }
+    //
+    // });
+
 }
 
 /*
