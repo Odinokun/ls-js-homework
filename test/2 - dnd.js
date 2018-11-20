6df50fdcb8244c1266e35d1a075a76dbf92f73b4
odinokun <odinokun@gmail.com>
2018-11-20 17:52:05 +0200

    update

--- test/2 - dnd.js
+++ test/2 - dnd.js
@@ -1,5 +1,5 @@
 import assert from 'assert';
-let template = require('../../../../garbidge/228_dz5/dnd-content.hbs');
+let template = require('../dnd-content.hbs');
 
 describe('ДЗ 5.2 - Div D&D', () => {
     let homeworkContainer = document.createElement('div');
